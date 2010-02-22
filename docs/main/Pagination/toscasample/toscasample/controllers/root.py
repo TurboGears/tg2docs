293864c30724cdba4ac7298c13db68d4704db838
Yannick Gingras <ygingras@ygingras.net>
2010-02-22 16:41:59 -0500

    added pagination with decorator to the example project
    --HG--
    branch : 2.1

--- docs/main/Pagination/toscasample/toscasample/controllers/root.py
+++ docs/main/Pagination/toscasample/toscasample/controllers/root.py
@@ -1,17 +1,18 @@
 """Main Controller"""
 
 from toscasample.lib.base import BaseController
-from tg import expose, flash, require, url, request, redirect
+from tg import expose, flash, require, redirect
 from pylons.i18n import ugettext as _, lazy_ugettext as l_
-from toscasample.controllers.error import ErrorController
-
-##{import}
-from tg import tmpl_context, redirect, validate
-from toscasample.model import metadata, DBSession, Movie
-from toscasample.widgets.movie_form_6 import create_movie_form
-##
 
 from webhelpers import paginate
+from tg.decorators import paginate as paginatedeco
+
+from tg import tmpl_context, validate
+
+from toscasample.controllers.error import ErrorController
+from toscasample.model import DBSession, Movie
+from toscasample.widgets.movie_form_6 import create_movie_form
+
 
 class RootController(BaseController):
     error = ErrorController()
@@ -20,25 +21,30 @@ class RootController(BaseController):
     def index(self):
         redirect("list")
 
-    ##{list}
     @expose("toscasample.templates.movie_list")
     def list(self, page=1):
         """List and paginate all movies in the database"""
         movies = DBSession.query(Movie)
         currentPage = paginate.Page(movies, page, items_per_page=5)
-        return dict(movies=currentPage.items, page='ToscaSample Movie list', currentPage=currentPage)
-    ##
+        return dict(movies=currentPage.items, page='ToscaSample Movie list', 
+                    currentPage=currentPage)
+    
+    @paginatedeco("movies", items_per_page=5)
+    @expose("toscasample.templates.movie_list_deco")
+    def decolist(self):
+        """List and paginate all movies in the database using the
+        paginate() decorator."""
+        movies = DBSession.query(Movie)
+        return dict(movies=movies, page='ToscaSample Movie list')
 
-    ##{new}
     @expose('toscasample.templates.new_form')
     def new(self, **kw):
         """Show form to add new movie data record."""
         tmpl_context.form = create_movie_form
         return dict(modelname='Movie',
             page='ToscaSample New Movie')
-    ##
 
-    ##{create}
+
     @validate(create_movie_form, error_handler=new)
     @expose()
     def create(self, **kw):
@@ -52,4 +58,3 @@ class RootController(BaseController):
         DBSession.add(movie)
         flash("Movie was successfully created.")
         redirect("list")
-    ##
