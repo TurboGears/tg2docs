2ce485b956e55fc6237241c8c8659ebd785df44a
Michael J. Pedersen <m.pedersen@icelus.org>
2011-12-11 13:40:52 -0500

    Final changes for 2.1.4 before release.

--- book/setup.py
+++ book/setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='TurboGears2Docs',
-    version='2.1.3',
+    version='2.1.4',
     author='TurboGears Community',
     url='http://www.turbogears.org/',
     license='MIT',
@@ -15,6 +15,6 @@ setup(
         'dulwich',
     ],
     dependency_links=[
-        "http://www.turbogears.org/2.1/downloads/next/"
+        "http://www.turbogears.org/2.1/downloads/current/"
         ]
 )
