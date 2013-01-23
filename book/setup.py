7add5cdad8f0b029d40625fd7f82ae39ce0c38c6
Michael J. Pedersen <m.pedersen@icelus.org>
2013-01-23 00:10:15 -0500

    Updating version number for release and final testing purposes.

--- book/setup.py
+++ book/setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='TurboGears2Docs',
-    version='2.2.0',
+    version='2.2.1',
     author='TurboGears Community',
     url='http://www.turbogears.org/',
     license='MIT',
@@ -15,6 +15,6 @@ setup(
         'dulwich',
     ],
     dependency_links=[
-        "http://tg.gy/220"
+        "http://tg.gy/221"
         ]
 )
